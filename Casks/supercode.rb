c37d2005988006cfe5706c37890da586be88cfa4
Yash Dewasthale <yashdev.yvd@gmail.com>
2026-09-23 22:36:30 +0530

    chore: update Supercode Desktop to 0.1.2

--- Casks/supercode.rb
+++ Casks/supercode.rb
@@ -1,11 +1,11 @@
 cask "supercode" do
-  version "0.1.1"
-  sha256 "e7d29ce482988556765e4220935e925bff7365be6bfece8ead99869459f734ea"
+  version "0.1.2"
+  sha256 "b5e9a1146f3fcf8d050a26c78512907356f768c3b035e2dbd4e44b3f2317280e"
 
   url "https://github.com/yashdev9274/supercli/releases/download/desktop-v#{version}/Supercode-#{version}-universal-development.dmg"
   name "Supercode"
-  desc "Native macOS coding agent"
-  homepage "https://supercode-terminal.vercel.app"
+  desc "Native coding agent"
+  homepage "https://supercode-terminal.vercel.app/"
 
   depends_on macos: :sonoma
 
